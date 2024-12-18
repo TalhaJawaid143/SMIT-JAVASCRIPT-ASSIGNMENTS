bd6d02445ae3b2f0d2444c95535f14a8210e773e
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-18 09:53:05 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,68 @@
+// Assignment01
+
+// Chapter Number01
+
+
+// Task 1
+alert("Hello!Thanks for your visiying our website")
+
+// Task 2
+alert("Please!Enter a valid password")
+
+// Task 3
+alert("Welcome to JS land\nHappy Coding!");
+
+// Chapter Number02
+
+// Task 1: Declare a variable called username
+var username;
+
+// Task 2: Declare a variable called myName & assign your full name
+var myName = "John Doe";
+
+// Task 3: Script to display a message in an alert box
+// a) Declare a JS variable titled message
+var message;
+
+// b) Assign “Hello World” to variable message
+message = "Hello World";
+
+// c) Display the message in an alert box
+alert(message);
+
+// Task 4: Script to save student’s bio data in JS variables and display them in alert boxes
+var studentName = "TALHA"; // Student's name
+var studentAge = "17 years old"; // Student's age
+var studentCourse = "Web Development"; // Student's course
+var studentEmail = "student@123"
+// Displaying student bio data in alert boxes
+alert("Name: " + studentName);
+alert("Age: " + studentAge);
+alert("Course: " + studentCourse);
+alert("Email: " + studentEmail);
+
+
+// Chapter Number03
+
+// Task 1:Declare a variable called age & assign to it your age.
+
+var a =  17;
+
+// Task 2:2. Declare & initialize a variable to keep track of how many times a visitor has visited a web page.
+
+var b = "You have visited this site 14 times";
+
+// Task 3: Declare a variable called birthYear
+var c = "Your Birth Date is"
+
+// Task 4:A visitor visits an online clothing store a. Visitor’s name b. Product title
+// c. Quantity i.e. how many products a visitor wants to
+// order
+
+var CustomerName = "Jhon Doe\n25\nOrdered 5 T-Shirts\nOn X-Y-Z Clothing"
+
+// Displaying Using Alert Messages
+alert(a);
+alert(b);
+alert(c);
+alert(CustomerName);
