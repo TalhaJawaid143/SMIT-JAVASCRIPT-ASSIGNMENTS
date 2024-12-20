83f6989b9259f09d6f5a23d2659b2692476c8dd3
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-20 12:31:48 +0500

    Add files via upload

---
+++ assignment2/main.js
@@ -0,0 +1,85 @@
+// CHAPTER NUMBER04
+
+
+
+document.write("1) Variables names can only contain numbers and $ _ For example: $my_1st variable <br />")
+document.write("2) Variables must begain with a letter and $ _ For example: $name, _name or name <br />")
+document.write("3) Variables names are case sensative <br />")
+document.write("4) Variables names should not be js keyword <br />")
+
+// CHAPTER NUMBER05
+
+// Task Number 01
+
+let num0 = 3;
+let num1 = 5;
+
+let sum = num0 + num1;
+
+document.write("Sum of " + num0 + " and " + num1 + " is: " + sum + "<br />")
+
+// Task Number 02
+
+let num2 = 5;
+let num3 = 7;
+
+let difference = num2 - num3;
+
+document.write("Difference of " + num2 + " and " + num3 + " is: " + difference + "<br />")
+
+// Task Number 03
+
+let num4 = 3;
+let num5 = 7;
+
+let product = num4 * num5;
+
+document.write("Product of " + num4 + " and " + num5 + " is: " + product + "<br />")
+
+// Task Number 04
+
+let num6 = 7;
+let num7 = 3;
+
+let quotient = num6 / num7;
+
+document.write("Quotient of " + num6 + " and " + num7 + " is: " + quotient + "<br />")
+
+// Task Number 05
+
+let TicketPrice1 = 600
+let TicketPrice5 = 5 * 600
+
+document.write("Total Cost To Buy 5 Tickets " + TicketPrice1 + " and " + TicketPrice5 +  " is: " + TicketPrice5 + "<br />")
+
+// Task Number 06
+
+var number = 4;
+document.write(number + " x 1 = " + (number * 1) + "<br>");
+document.write(number + " x 2 = " + (number * 2) + "<br>");
+document.write(number + " x 3 = " + (number * 3) + "<br>");
+document.write(number + " x 4 = " + (number * 4) + "<br>");
+document.write(number + " x 5 = " + (number * 5) + "<br>");
+document.write(number + " x 6 = " + (number * 6) + "<br>");
+document.write(number + " x 7 = " + (number * 7) + "<br>");
+document.write(number + " x 8 = " + (number * 8) + "<br>");
+document.write(number + " x 9 = " + (number * 9) + "<br>");
+document.write(number + " x 10 = " + (number * 10) + "<br>");
+
+
+// CHAPTER NUMBER 07
+
+// Task Number 01
+
+let myName0 = "Rsult";
+document.write(myName0 + "<br />")
+
+let myName1 = "The Value of a is :10";
+document.write(myName1 + "<br />")
+
+let myName = 11
+let myName2 = "The value of ++a is :11";
+document.write(myName2 + "<br />")
+
+let myName02 = "The Value of --a is :1";
+document.write(myName02 + "<br />")
