fb162d370f0a25ed6f70df4addce49e9a14d1c91
Talha Jawaid <talhajawaid601@gmail.com>
2024-12-28 14:30:36 +0500

    Add files via upload

---
+++ assignment4/script.js
@@ -0,0 +1,68 @@
+// // Task number1
+
+// const studentName = [];
+
+// // Task number2
+
+// const studentNames = [
+//      name1 = "jhon",
+//      name2 = "sarah",
+//      name3 = "mike",
+//      name4 = "emily",
+//      name5 = "laura",
+// ];
+// console.log(studentNames[0]);
+
+
+// // Task number3
+
+// let fruits = [
+//     { name: "apple", price: "20 dollars", quantity: 2 },
+//     { name: "banana", price: "15 dollars", quantity: 3 },
+//     { name: "orange", price: "18 dollars", quantity: 1 },
+//     { name: "grape", price: "25 dollars", quantity: 4 },
+//     { name: "kiwi", price: "10 dollars", quantity: 5 },
+// ];
+
+// // Task number4
+
+// let StudentsGrade = [
+//     { name: "John", grade: 95 },
+//     { name: "Sarah", grade: 88 },
+//     { name: "Mike", grade: 92 },
+//     { name: "Emily", grade: 90 },
+//     { name: "Laura", grade: 85 },
+// ];
+
+// // Task number5
+
+// let booleans = [
+//    {jhonGrade: true},
+//    {SarahGrade: false},
+// ];
+
+// // Task number6
+
+// let mixStrings = [
+//     {name1 : jhon,Grade: true, age:20},
+//     {name2 : sarah,Grade: false, age:22},
+//     {name3 : mike,Grade: false, age:24},
+//     {name4 : emily,Grade:true, age:26},
+//     {name5 : laura,Grade:true, age:28},
+// ];
+
+// console.log(mixStrings);
+
+// // Task number7
+//  // Declare and initialize the array
+//  let qualifications = ["SSC", "HSC", "BCS", "BS", "BCOM", "MS", "M. Phil.", "PhD"];
+
+//  // Use document.write to display the qualifications
+//  document.write("<ul>");
+//  qualifications.forEach(qualification => {
+//    document.write("<li>" + qualification + "</li>");
+//  });
+//  document.write("</ul>");
+
+//   console.log(qualifications);
+
